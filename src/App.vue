536b4af283e9561fd607aa2498b574fe2c1d3cd3
abedaliby <abedaliby@yahoo.com>
2018-08-23 18:25:02 +0200

    style changes to h1

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,36 @@
 <template>
   <div id="app">
     <navbar></navbar>
-<h1>Mohamed</h1>
-<router-view></router-view>
-<icon name="github"></icon>
+    <h1>Mohamed</h1>
+    <router-view></router-view>
+    <icon name="github"></icon>
   </div>
 </template>
 
 <script>
 import navbar from "./components/navbar.vue";
 
-
 export default {
   name: "app",
-  components:{
+  components: {
     navbar
   },
   data() {
-    return {
-    };
+    return {};
   }
 };
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+h1 {
+  font-size: 2em;
 }
 ul {
   margin: 0;
